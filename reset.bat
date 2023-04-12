c133773f7112ef8d0c5f5afcbaea1fc156926baf
abdurrahmanbeg <abdurrahman@outlook.com>
2023-04-13 09:55:29 +1000

    Added comments

--- reset.bat
+++ reset.bat
@@ -2,13 +2,14 @@
 
 taskkill /F /IM chrome.exe /T
 
-
+:: Set Paths :: 
 set ChromeDir=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data
 set Documents=C:\Users\%USERNAME%\Documents\
 set Desktop=C:\Users\%USERNAME%\Desktop\
 set Pictures=C:\Users\%USERNAME%\Pictures\
-set Downloads= C:\Users\%USERNAME%\Downloads\
+set Downloads=C:\Users\%USERNAME%\Downloads\
 
+:: Delete Files ::
 del /q /s /f "%ChromeDir%"
 rd /s /q "%ChromeDir%"
 
@@ -20,9 +21,13 @@ del /q /s /f "%Downloads%"
 del /F /Q %APPDATA%\Microsoft\Windows\Recent\*
 del /F /Q %APPDATA%\Microsoft\Windows\Recent\AutomaticDestinations\*
 del /F /Q %APPDATA%\Microsoft\Windows\Recent\CustomDestinations\*
+
+:: Restart Explorer ::
 taskkill /f /im explorer.exe
 start explorer.exe
 
+:: Volume Down to 0 ::
+    :: Creates temporary JavaScript file ::
 echo var oShell = new ActiveXObject("WScript.Shell"); >> vd.js
 set "line=for (var i=0; i<50; i++){"
 setlocal EnableDelayedExpansion
@@ -33,11 +38,9 @@ echo oShell.SendKeys(String.fromCharCode(0xAE)); >> vd.js
 echo } >> vd.js
 
 timeout /t 1
-
 cscript /e:jscript vd.js
-
 timeout /t 1
-
+     :: Deletes temporary JavaScript file ::
 del /F/ Q vd.js
 
 start chrome https://docs.google.com/forms/d/e/1FAIpQLSdpeyvOBks6QBCz8PJeVibY6jwf7VNbJ9GCxjv4CpvvuRl9ew/viewform

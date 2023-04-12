ff360f8c6d5f229605fd94f20e3ea60ec99d9d36
abdurrahmanbeg <abdurrahman@outlook.com>
2023-04-13 08:17:49 +1000

    Added del recent files

--- reset.bat
+++ reset.bat
@@ -2,11 +2,13 @@
 
 taskkill /F /IM chrome.exe /T
 
+
 set ChromeDir=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data
 set Documents=C:\Users\%USERNAME%\Documents\
 set Desktop=C:\Users\%USERNAME%\Desktop\
 set Pictures=C:\Users\%USERNAME%\Pictures\
 set Downloads= C:\Users\%USERNAME%\Downloads\
+set Recents= %AppData%\Microsoft\Windows\Recent\
 
 del /q /s /f "%ChromeDir%"
 rd /s /q "%ChromeDir%"
@@ -16,5 +18,10 @@ del /q /s /f "%Desktop%"
 del /q /s /f "%Pictures%"
 del /q /s /f "%Downloads%"
 
+del /F /Q %APPDATA%\Microsoft\Windows\Recent\*
+del /F /Q %APPDATA%\Microsoft\Windows\Recent\AutomaticDestinations\*
+del /F /Q %APPDATA%\Microsoft\Windows\Recent\CustomDestinations\*
+taskkill /f /im explorer.exe
+start explorer.exe
 
 start chrome https://docs.google.com/forms/d/e/1FAIpQLSdpeyvOBks6QBCz8PJeVibY6jwf7VNbJ9GCxjv4CpvvuRl9ew/viewform
